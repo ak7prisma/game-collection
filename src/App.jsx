9eed76ba3c0e4fee8d278795abbd63941eecb334
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-03-24 13:40:10 +0700

    chore: install-react-icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import MainSection from "./components/MainSection"
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
+      <MainSection />
+      <Footer />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 export default function Navbar(){
     return(
-        <section>
-            <div>
-                
-            </div>
-            <div>
-
-            </div>
-            <div>
-                
-            </div>
-        </section>
+        <nav className="fixed flex justify-center bottom-2 bg-blue-300 p-2 w-full">
+            <ul className="flex">
+                <li>
+                    
+                </li>
+                <li>
+                    aku
+                </li>
+                <li>
+                    aku
+                </li>
+            </ul>
+        </nav>
     );
 }
