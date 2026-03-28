16e3649ebcfd465ed80ba0d5885522f65924b550
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-03-29 04:01:47 +0700

    docs: add-deployment

--- src/data/navItems.js
+++ src/data/navItems.js
@@ -2,6 +2,6 @@ import { IoCall, IoGameController, IoHome } from "react-icons/io5";
 
 export const navItems = [
   { id: 1, name: "Games", link: "#games",  icon: IoGameController },
-  { id: 2, name: "Home", link: "#home", icon: IoHome },
+  { id: 2, name: "Home", link: "#", icon: IoHome },
   { id: 3, name: "Contact", link: "https://portofolio-ahmad-kurnia-prisma.vercel.app/#contact", icon: IoCall },
 ];
